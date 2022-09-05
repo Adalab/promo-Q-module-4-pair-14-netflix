53a79198b967829365a2865c5faaf37fa9bcef89
NadiaMonzon <nadiamonque99@gmail.com>
2022-09-05 10:11:43 +0100

    Try to filter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const movies = require('./data/movies.json');
 
 // create and config server
 const server = express();
@@ -10,4 +11,14 @@ server.use(express.json());
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+server.get('/movies', (req, resp)=>{
+  console.log(req.query);
+  const gender = req.query.gender;
+  
+  resp.json({
+    movies,
+    success: true
+  })
 });
